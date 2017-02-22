164a4c6aad640d5652bef1d3546bf74a5ceeb5b6
Thomas Grottker <thomas.grottker@webtrekk.com>
2017-02-22 22:15:01 +0100

    add logic to keep cells alive to nextGeneration function in Cell enum

--- src/main/java/com/gfk/conway/Cell.java
+++ src/main/java/com/gfk/conway/Cell.java
@@ -4,6 +4,9 @@ public enum Cell {
     Alive, Dead;
 
     public Cell nextGeneration(int neighbours) {
+        if (neighbours == 2 || neighbours == 3) {
+            return Alive;
+        }
         return Dead;
     }
 }
