b7cb93d80bd0ca910359aa3d508a7b1599e36f5c
Thomas Grottker <thomas.grottker@webtrekk.com>
2017-02-22 22:53:35 +0100

    add first test for counting neighbours

---
+++ src/test/java/com/gfk/conway/GenerationTest.java
@@ -0,0 +1,39 @@
+package com.gfk.conway;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GenerationTest {
+
+    Board board;
+
+    /**
+     * This function creates the following board:
+     *
+     *  | |
+     * -|-|-
+     *  | |X
+     * -|-|-
+     * X|X|X
+     * -|-|-
+     *  | |X
+     *
+     */
+    @Before
+    public void createBoard() {
+        Cell[] firstRow = {Cell.Dead, Cell.Dead, Cell.Dead};
+        Cell[] secondRow = {Cell.Dead, Cell.Dead, Cell.Alive};
+        Cell[] thirdRow = {Cell.Alive, Cell.Alive, Cell.Alive};
+        Cell[] fourthRow = {Cell.Dead, Cell.Dead, Cell.Alive};
+        Cell[][] population = {firstRow, secondRow, thirdRow, fourthRow};
+        this.board = new Board(population);
+    }
+
+    @Test
+    public void countNeighbours() {
+        assertEquals(0, board.countNeigbours(0, 0));
+    }
+
+}
