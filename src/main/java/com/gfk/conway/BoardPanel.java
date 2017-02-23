389fae3610e4f1de66a88256585cee2c1349ae5a
Thomas Grottker <thomas.grottker@webtrekk.com>
2017-02-23 10:40:41 +0100

    add drawing of living cells

--- src/main/java/com/gfk/conway/BoardPanel.java
+++ src/main/java/com/gfk/conway/BoardPanel.java
@@ -5,6 +5,8 @@ import java.awt.*;
 
 public class BoardPanel extends JPanel {
 
+    private Board board = new Board(50, 50);
+
     public BoardPanel() {
         setBorder(BorderFactory.createLineBorder(Color.BLACK));
         setPreferredSize(new Dimension(500, 500));
@@ -12,12 +14,28 @@ public class BoardPanel extends JPanel {
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+        drawCells(g);
         drawGird(g);
+    }
+
+    private void drawCells(Graphics g) {
+        Cell[][] population = board.population;
+        int height = population.length;
+        int width = population[0].length;
+        g.setColor(Color.GREEN);
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (Cell.isAlive(population[y][x])) {
+                    g.fillRect(x*10, y*10, 10, 10);
+                }
+            }
+        }
     }
 
     private void drawGird(Graphics g) {
         int width = getWidth();
         int height = getHeight();
+        g.setColor(Color.BLACK);
         for (int i = 0; i < width; i += 10) {
             g.drawLine(i, 0, i, height);
         }
